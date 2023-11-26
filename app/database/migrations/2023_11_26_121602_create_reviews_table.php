fbb51d77805803ddde38c0a89c8659ef508381b9
mdrobel <robel@revo-interactive.com>
2023-11-26 18:29:04 +0600

    model migration & controller created for review moduel

---
+++ app/database/migrations/2023_11_26_121602_create_reviews_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('company_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('product_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->text('comment');
+            $table->unsignedTinyInteger('point');
+            $table->unsignedTinyInteger('like')->default(0)->nullable();
+            $table->unsignedTinyInteger('dislike')->default(0)->nullable();
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
