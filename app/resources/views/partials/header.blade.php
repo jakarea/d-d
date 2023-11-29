cd3d8a91de654c96fed9d106e90216f01e7b8572
Akram Hossain <nayanbd17@gmail.com>
2023-11-29 12:47:59 +0600

    responsive menu header done

--- app/resources/views/partials/header.blade.php
+++ app/resources/views/partials/header.blade.php
@@ -21,29 +21,36 @@
             </li>
             <li>
                 <div class="dropdown p-0 header-dropdown">
-                    <a class="p-0" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="p-0 user" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         <img src="{{ asset('assets/images/user.png') }}" alt="B" class="img-fluid">
                         <i class="fas fa-angle-down ms-2"></i>
                     </a>
 
                     <ul class="dropdown-menu">
                         <li>
-                            <a class="dropdown-item {{ Request::is('account/my-profile') ? 'active' : ''}}" href="{{url('account/my-profile')}}">Profile 
+                            <a class="dropdown-item {{ Request::is('account/my-profile') ? 'active' : ''}}"
+                                href="{{url('account/my-profile')}}">Profile
                                 @if (Request::is('account/my-profile'))
-                                    <i class="fas fa-check"></i> 
-                                @endif 
-                            </a>
-                        </li> 
-                        <li>
-                            <a class="dropdown-item  {{ Request::is('account/edit-profile') ? 'active' : ''}}" href="{{url('account/edit-profile')}}">
-                                Profile Setting 
-                                @if (Request::is('account/edit-profile'))
-                                    <i class="fas fa-check"></i> 
+                                <i class="fas fa-check"></i>
                                 @endif
                             </a>
-                        </li> 
+                        </li>
+                        <li>
+                            <a class="dropdown-item  {{ Request::is('account/edit-profile') ? 'active' : ''}}"
+                                href="{{url('account/edit-profile')}}">
+                                Profile Setting
+                                @if (Request::is('account/edit-profile'))
+                                <i class="fas fa-check"></i>
+                                @endif
+                            </a>
+                        </li>
                     </ul>
                 </div>
+            </li>
+            <li>
+                <a href="#" class="d-lg-none" id="menuToggle">
+                    <i class="fas fa-bars"></i>
+                </a>
             </li>
         </ul>
     </div>
